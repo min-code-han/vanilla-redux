6c478885d887d4618402a1e59f0176958b26eb65
mina <yegrinaaa@gmail.com>
2024-01-31 19:15:18 +0900

    action 관리 리팩토링

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ const number = document.querySelector("span")
 
 number.innerText = 0
 
+const ADD = "ADD"
+const MINUS = "MINUS"
+
 // reducer
 const countModifier = (count = 0, action) => {
     switch (action.type) {
-        case "ADD":
+        case ADD:
             return count + 1
-        case "MINUS":
+        case MINUS:
             return count - 1
         default:
             return count
@@ -22,8 +25,8 @@ const countModifier = (count = 0, action) => {
 // store
 const countStore = createStore(countModifier)
 
-add.addEventListener('click', () => countStore.dispatch({type: "ADD"}))
-minus.addEventListener('click', () => countStore.dispatch({type: "MINUS"}))
+add.addEventListener('click', () => countStore.dispatch({type: ADD}))
+minus.addEventListener('click', () => countStore.dispatch({type: MINUS}))
 
 const onChange = () => {
     const _count = countStore.getState()
