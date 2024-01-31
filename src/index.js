72639facac22cfdc0793b3d80f6cd63bf9d6fd93
mina <yegrinaaa@gmail.com>
2024-01-31 19:08:16 +0900

    기존 vanille javaScript -> redux 로 변경

--- src/index.js
+++ src/index.js
@@ -4,13 +4,30 @@ const add = document.getElementById("add")
 const minus = document.getElementById("minus")
 const number = document.querySelector("span")
 
+number.innerText = 0
+
 // reducer
-const countModifier = (count = 0) => {
-    return count
+const countModifier = (count = 0, action) => {
+    switch (action.type) {
+        case "ADD":
+            return count + 1
+        case "MINUS":
+            return count - 1
+        default:
+            return count
+    }
 }
 
 
 // store
 const countStore = createStore(countModifier)
 
+add.addEventListener('click', () => countStore.dispatch({type: "ADD"}))
+minus.addEventListener('click', () => countStore.dispatch({type: "MINUS"}))
 
+const onChange = () => {
+    const _count = countStore.getState()
+    number.innerText = _count
+}
+
+countStore.subscribe(onChange)
